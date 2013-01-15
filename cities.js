5674d20a11857aa08dde4b7b3d3fe0b1b1fde7a1
kwlczk <astrid.kowalczyk@hyperisland.se>
2013-01-15 12:58:11 +0100

    Added glow again

--- cities.js
+++ cities.js
@@ -43,7 +43,7 @@ function createCitiesLayer(cities) {
         sphere = new O3D.Sphere({
           nlat: 5,
           nlong: 5,
-          radius: 1 / 200,
+          radius: 1 / 100,
           pickingColors: [100, g / 55, b / 100, 1]
         }),
         tvertices = slice.call(sphere.vertices).map(function(v, i) { return coords[i % 3] + v; });
